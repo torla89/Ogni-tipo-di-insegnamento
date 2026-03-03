30fce8dd2dd68282839783b7f796b6da7b1fbb08
Torlai <torla.89@hotmail.it>
2026-03-03 15:33:27 +0100

    aggiunte autorizzazioni Android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.ognitipodiinsegnamento"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 16
-        versionName = "26.03.02"
+        versionCode = 17
+        versionName = "26.03.03"
     }
 
     buildTypes {
