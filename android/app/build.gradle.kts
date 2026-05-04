b75008338d4806c2ca8c9abbf9133213e0f16796
Torlai <torla.89@hotmail.it>
2026-05-04 12:02:08 +0200

    Fix URL audio studi: nuovo archivio ogni-tipo-di-insegnamento-letture-fixed

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.ognitipodiinsegnamento"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 31
-        versionName = "26.05.01"
+        versionCode = 32
+        versionName = "26.05.02"
     }
 
     buildTypes {
